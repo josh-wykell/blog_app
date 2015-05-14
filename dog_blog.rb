b1316d853a024eea5f8061ba4f89bff9d06569f1
Josh Wykell <jwykell@gmail.com>
2015-05-14 16:23:17 -0400

    first web app

---
+++ dog_blog.rb
@@ -0,0 +1,21 @@
+require 'erb'
+require 'webrick'
+require 'yaml'
+
+ROOT = File.dirname(__FILE__)
+
+server = WEBrick::HTTPServer.new(:Port => 8000)
+
+server.mount '/assets', WEBrick::HTTPServlet::FileHandler, "#{ROOT}/public"
+
+server.mount_proc '/' do |req, res|
+  @data = YAML.load_file('dog_blog.yml')
+  template = ERB.new(File.read("#{ROOT}/index1.html.erb"))
+  res.body = template.result
+end
+
+trap 'INT' do
+  server.shutdown
+end
+
+server.start
